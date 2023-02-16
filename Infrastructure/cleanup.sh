47e6405a6e84aefbe8f66afdcc6f1fb282b270e3
Lasse <lasse.appel@gmail.com>
2023-02-16 01:35:00 +0100

    Add cleanup script

---
+++ Infrastructure/cleanup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+az group delete -n $RESOURCE_GRP --force-deletion-types Microsoft.Compute/virtualMachines
--- Infrastructure/env.sh
+++ Infrastructure/env.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-export IP='20.93.19.252'
+export IP='20.93.21.25'
 export USR='azureuser'
-export KEY='/Users/lasse/Downloads/App_key.pem'
-export SERVER_NAME='App'
-export RESOURCE_GRP='App'
+export KEY='/Users/lasse/Downloads/Demo_key.pem'
+export SERVER_NAME='Demo'
+export RESOURCE_GRP='Demo'
 
 export APP_NAME='MyFirstApp123'
 export TARGET_DIR="/opt/$APP_NAME"
